087be0c03e7bbaaeb53e5f1c12b160f230ceea6c
Zachary Chai <zachary.chai@outlook.com>
2016-06-15 23:05:35 -0400

    check if config file exist before loading

--- lib/cloudit/config.rb
+++ lib/cloudit/config.rb
@@ -2,12 +2,17 @@ require 'yaml'
 
 class Cloudit::Config
   ATTRIBUTES = [:region, :access_key_id, :secret_access_key]
+  FILE_NAME = "./.cloudit/config"
 
   class << self
     attr_accessor(*ATTRIBUTES)
   end
 
-  yml = YAML::load_file("./.cloudit/config")
+  yml = if File.exist? FILE_NAME
+    YAML::load_file(FILE_NAME)
+  else
+    {}
+  end
 
   ATTRIBUTES.each do |a|
     self.send("#{a.to_s}=", yml[a.to_s])
